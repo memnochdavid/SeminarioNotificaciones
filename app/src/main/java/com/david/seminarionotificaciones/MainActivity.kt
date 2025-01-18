a50e7d4b7c749b0e36ca0dcbe4fa480f3287efcf
David Duque <davduquediaz@gmail.com>
2025-01-18 10:06:48 +0100

    Initial commit

---
+++ app/src/main/java/com/david/seminarionotificaciones/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.david.seminarionotificaciones
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getString
+import androidx.core.content.ContextCompat.getSystemService
+import android.Manifest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.core.content.ContextCompat
+import com.david.seminarionotificaciones.ui.theme.SeminarioNotificacionesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SeminarioNotificacionesTheme {
+                SeminarioNotificaciones()
+            }
+        }
+    }
+}
+
+@Composable
+fun SeminarioNotificaciones() {
+    val context = LocalContext.current
+    val channelId = "my_channel_id" // Use a descriptive ID
+    var notificationId by remember { mutableStateOf(0) }
+    val title = "NOTIFICACIÓN, TRONCO"
+    val text = "Hola picha!"
+    val texto_largo=getString(context,R.string.lorem)
+
+    LaunchedEffect(Unit) {
+        createNotificationChannel(context)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // ejercicio 01
+        Button(
+            onClick = {
+
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                    sendNotification(context, channelId, notificationId, title, text,1)
+                    notificationId++
+                } else {
+
+                    ActivityCompat.requestPermissions(context as ComponentActivity, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0)
+                }
+            }
+        ) {
+            Text(text = "Ejercicio 01")
+        }
+        //ejercicio 2 - 1
+        Button(
+            onClick = {
+
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                    sendNotification(context, channelId, notificationId, title, text,21)
+                    notificationId++
+                } else {
+
+                    ActivityCompat.requestPermissions(context as ComponentActivity, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0)
+                }
+            }
+        ) {
+            Text(text = "Ejercicio 02 - BigPictureStyle")
+        }
+        //ejercicio 2 - 2
+        Button(
+            onClick = {
+
+                if (ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                    sendNotification(context, channelId, notificationId, title, texto_largo,22)
+                    notificationId++
+                } else {
+
+                    ActivityCompat.requestPermissions(context as ComponentActivity, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 0)
+                }
+            }
+        ) {
+            Text(text = "Ejercicio 02 - BigPictureStyle")
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/david/seminarionotificaciones/Notificacion.kt
@@ -0,0 +1,120 @@
+package com.david.seminarionotificaciones
+
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+
+fun sendNotification(
+    context: Context,
+    channelId: String,
+    notificationId: Int,
+    title: String,
+    text: String,
+    opc:Int
+) {
+    val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        putExtra("message", "Hello from notification!")
+    }
+    val pendingIntent: PendingIntent = PendingIntent.getActivity(
+        context,
+        notificationId,
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+
+    val builder = when(opc){
+        1 ->{//ej01
+            NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(R.drawable.dick)
+                .setLargeIcon(
+                    BitmapFactory.decodeResource(
+                        context.resources,
+                        R.drawable.dick
+                    )
+                )
+                .setContentTitle(title)
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.dick, "Abrir", pendingIntent)
+        }
+        21 ->{
+            NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(R.drawable.dick)
+                .setLargeIcon(
+                    BitmapFactory.decodeResource(
+                        context.resources,
+                        R.drawable.banner
+                    )
+                )
+                .setContentTitle(title)
+                .setStyle(NotificationCompat.BigPictureStyle()
+                    .bigPicture( BitmapFactory.decodeResource(context.resources, R.drawable.banner)))
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+        }
+        22 ->{
+            NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(R.drawable.dick)
+                .setLargeIcon(
+                    BitmapFactory.decodeResource(
+                        context.resources,
+                        R.drawable.dick
+                    )
+                )
+                .setContentTitle(title)
+                .setStyle(NotificationCompat.BigTextStyle().bigText(text))
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+        }
+        else -> {
+            NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(R.drawable.dick)
+        }
+    }
+
+    with(NotificationManagerCompat.from(context)) {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notify(notificationId, builder.build())
+    }
+}
+
+fun createNotificationChannel(context: Context) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channelId = "my_channel_id"
+        val name = context.getString(R.string.channel_name)
+        val descriptionText = context.getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(channelId, name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+}
